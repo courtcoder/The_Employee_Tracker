912ab4d6a926cc2f8af2f98ad11ec4dc450cca59
courtcoder <courtney1187@icloud.com>
2022-07-08 20:02:40 -0400

    worked on server.js file

--- server.js
+++ server.js
@@ -84,3 +84,97 @@ function viewAllEmployees() {
     })
 };
 
+function viewRoles() {
+    let request = 'SELECT * FROM role';
+    db.query(request, function (err, res) {
+        if (err) throw err;
+        console.log("Viewing Roles");
+        console.table(res);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Select an Option.',
+                choices: [
+                    'Main Menu',
+                    'Quit'
+                ],
+            }
+        ])
+            .then((answer) => {
+                switch (answer.choice) {
+                    case 'Main Menu':
+                        start();
+                        break;
+                    case 'Quit':
+                        quit();
+                        break;
+                }
+            }
+            )
+    })
+}
+
+function viewDepartments() {
+    const request = 'SELECT * FROM department';
+    db.query(request, function (err, res) {
+        if (err) throw err;
+        console.log("Viewing Departments");
+        console.table(res);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Select an Option.',
+                choices: [
+                    'Main Menu',
+                    'Quit'
+                ],
+            }
+        ])
+            .then((answer) => {
+                switch (answer.choice) {
+                    case 'Main Menu':
+                        start();
+                        break;
+                    case 'Quit':
+                        quit();
+                        break;
+                }
+            }
+            )
+    })
+}
+
+//  add new employee 
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter the first name of the employee.'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter the last name of the employee.'
+        },
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'Enter the id of the employee.'
+        }
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'Enter their managers id.'
+        }
+    ])
+        .then(function (response) {
+            connection.query('INSERT INTO employees(first_name, last_name, employee_id, manager_id) VALUES(?,?,?,?)', [response.first_name, response.last_name, response.employee_id, response.manager_id], function (err, res) {
+                if (err) throw err;
+                console.table(res);
+
+                start();
+            }
+        }
